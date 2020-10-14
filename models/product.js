9f545ac1887a9b5f45a11a3476de096417c925b8
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-10-14 17:48:53 -0300

    Refactor models module

--- models/product.js
+++ models/product.js
@@ -1,4 +1,18 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const rootDir = require("../util/path");
+
+const filePath = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(filePath, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +20,16 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+
+      fs.writeFile(filePath, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
